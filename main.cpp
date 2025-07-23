38e080c7f346e10851f9d7a67da5a2a67b89deb3
hussein-momen <husseinjunior334@gmail.com>
2025-07-23 10:40:07 +0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int choice;
+    cout << "Typing Speed Test--Console Version";
+    cout << "1.Start Typing/n2.Highest Scores/n3.Learn To Type(Web)/n2.99.Exit";
+    cin >> choice;
+    if (choice == 1) {
+        // Main Logic
+    } else if (choice == 2)
+    {
+        // High Scores Should Be Saved Into A File I/O
+    } else if (choice == 3)
+    {
+        cout << "Lessons For Learning:/nLINKHERE.com" << endl;
+        cout << "Note: This Website Not Managed By Me...I just Want To Help :)" << endl;
+        cout << "Note: This Is Not An Advertisement...I choosed the best free source" << endl;
+    }
+}
